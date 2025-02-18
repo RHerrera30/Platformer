75584a899ca1825239af101cd3312a3454194287
RHerrera30 <152225484+RHerrera30@users.noreply.github.com>
2025-02-18 00:06:41 -0800

    Initial commit

---
+++ Assets/Platformer/Scripts/Game Manager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int timeLeft = 300 - (int)Time.time;
+        timerText.text = $"Time Left: {timeLeft}";
+    }
+}
