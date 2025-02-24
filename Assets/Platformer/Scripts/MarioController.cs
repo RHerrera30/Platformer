65b75dfb57a438d83f549eaca65146e3c96a2654
RHerrera30 <152225484+RHerrera30@users.noreply.github.com>
2025-02-23 21:16:21 -0800

    Fixed Mario's Lack of Run
    Changed the acceleration so the difference between walk and run is more evident. yay

--- Assets/Platformer/Scripts/MarioController.cs
+++ Assets/Platformer/Scripts/MarioController.cs
@@ -73,16 +73,6 @@ public class MarioController : MonoBehaviour
         {
             rb.AddForce(transform.up * jumpImpulse, ForceMode.VelocityChange);
         }
-        // if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        // {
-        //     rb.AddForce(Vector3.up * jumpImpulse, ForceMode.VelocityChange);
-        // } else if (Input.GetKey(KeyCode.Space) && !isGrounded)
-        // {
-        //     if (rb.linearVelocity.y > 0)
-        //     {
-        //         rb.AddForce(Vector3.up * jumpBoostForce, ForceMode.Acceleration);
-        //     }
-        // }
     }
 
     void FixedUpdate()
@@ -119,57 +109,4 @@ public class MarioController : MonoBehaviour
         UpdateFaceDirection();
         UpdateAnimation();
     }
-
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     float horizontalAmount = Input.GetAxis("Horizontal");
-    //     rb.linearVelocity += Vector3.right * (horizontalAmount * Time.deltaTime * acceleration);
-    //
-    //     float horizontalSpeed = rb.linearVelocity.x;
-    //     horizontalSpeed = Mathf.Clamp(horizontalSpeed, -maxSpeed, maxSpeed);
-    //
-    //     Vector3 newVelocity = rb.linearVelocity;
-    //     newVelocity.x = horizontalSpeed;
-    //     rb.linearVelocity = newVelocity;
-    //     
-    //     Collider c = GetComponent<Collider>();
-    //     Vector3 startPoint = transform.position;
-    //     float castDistance = c.bounds.extents.y + 0.01f;
-    //     
-    //     isGrounded = Physics.Raycast(startPoint, Vector3.down, castDistance);
-    //     
-    //     Color color = (isGrounded) ? Color.green : Color.red;
-    //     Debug.DrawLine(startPoint, startPoint + Vector3.down * castDistance, color, 0f, false);
-    //
-    //     if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-    //     {
-    //         rb.AddForce(Vector3.up * jumpImpulse, ForceMode.VelocityChange);
-    //     } else if (Input.GetKey(KeyCode.Space) && !isGrounded)
-    //     {
-    //         if (rb.linearVelocity.y > 0)
-    //         {
-    //             rb.AddForce(Vector3.up * jumpBoostForce, ForceMode.Acceleration);
-    //         }
-    //     }
-    //
-    //     // float verticalSpeed = Mathf.Clamp(rb.linearVelocity.y,maxFallSpeed, maxJumpSpeed);
-    //     // Vector3 clampedVelocity = rb.linearVelocity;
-    //     // clampedVelocity.y = verticalSpeed;
-    //     // rb.linearVelocity = clampedVelocity;
-    //
-    //     if (horizontalAmount == 0f)
-    //     {
-    //         Vector3 decayedVelocity = rb.linearVelocity;
-    //         decayedVelocity.x *= 1f - Time.deltaTime * 4f;
-    //         rb.linearVelocity = decayedVelocity;
-    //     }
-    //     else
-    //     {
-    //         float yawRotation = (horizontalAmount > 0f) ? 90f : -90f;
-    //         Quaternion rotation = Quaternion.Euler(0f, yawRotation, 0f);
-    //         transform.rotation = rotation;
-    //     }
-    //     UpdateAnimation();
-    // }
 }
